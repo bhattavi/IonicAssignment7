7a58859a4d07fc700af72d4fea77ef9eed4d73e6
abhinav <bhattabhinav@github.com>
2020-04-01 21:40:02 -0400

    boilerplate

--- src/app/folder/folder.module.ts
+++ src/app/folder/folder.module.ts
@@ -7,13 +7,15 @@ import { IonicModule } from '@ionic/angular';
 import { FolderPageRoutingModule } from './folder-routing.module';
 
 import { FolderPage } from './folder.page';
+import { ModalPageModule } from '../modal/modal.component';
 
 @NgModule({
   imports: [
     CommonModule,
     FormsModule,
     IonicModule,
-    FolderPageRoutingModule
+    FolderPageRoutingModule,
+    ModalPageModule
   ],
   declarations: [FolderPage]
 })
--- src/app/folder/folder.page.ts
+++ src/app/folder/folder.page.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { ModalController, ActionSheetController, LoadingController } from '@ionic/angular';
+import { ModalPage } from '../modal/modal.page';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-folder',
@@ -9,10 +12,62 @@ import { ActivatedRoute } from '@angular/router';
 export class FolderPage implements OnInit {
   public folder: string;
 
-  constructor(private activatedRoute: ActivatedRoute) { }
+  name: string;
+
+  constructor(private activatedRoute: ActivatedRoute, private modal: ModalController,
+    private actionSheetController: ActionSheetController,
+    private router: Router,
+    private loadingController: LoadingController) { }
 
   ngOnInit() {
     this.folder = this.activatedRoute.snapshot.paramMap.get('id');
+  }
+
+  async onModal() {
+
+    const modal = await this.modal.create({
+      component: ModalPage,
+      componentProps: {
+        data: this.name
+      }
+    });
+
+    await modal.present();
+
+
+  }
+
+  async actionSheet() {
+   
+
+    const actionSheet = this.actionSheetController.create({
+      header: 'Dragon Ball Z Characters',
+      buttons: [{
+        text: 'Goku',
+        role: 'Saiyan',
+        handler: () => {
+          console.log('Kamehamehaaaaaaaaa');
+
+        }
+      }, {
+        text: 'Vegeta',
+        role: 'Saiyan',
+        handler: () => {
+          console.log("YOU SAY I'M ARROGANT, I SAY DAMN RIGHT. THAT'S PRIDE. PRIDE IN THE SAIYAN I AM.");
+
+        }
+      }, {
+
+
+        text: 'Frieza',
+        role: 'Enemy',
+        handler: () => {
+          console.log('No Saiyan can be allowed to survive!');
+        }
+      }]
+    });
+
+    (await actionSheet).present();
   }
 
 }
